1ffe5a28b90f858bf41f6ae7a29de2a7443979a5
Gold <dzharzard96@gmail.com>
2021-04-10 23:11:42 -0500

    Bare Bones del Sistema de Dialogo.

---
+++ Pre-Mobius/Assets/Mobius/Scripts/Dialogue/ScriptableObjects/DialogScriptable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+[CreateAssetMenu(fileName = "NPCDialog", menuName = "NPC Dialogues", order = 1)]
+
+[System.Serializable]
+public class DialogScriptable : ScriptableObject
+{
+
+    public int npcID;
+    public string npcName;
+    public Message[] messages;
+
+
+
+
+
+}
+
+[System.Serializable]
+public class Message
+{
+    public string text;
+   
+}
+
+
+
+
+
---
+++ Pre-Mobius/Assets/Mobius/Scripts/Dialogue/ScriptableObjects/Farmer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Farmer : MonoBehaviour, IInteractable
+{
+    [SerializeField] GameObject imageBox = default;
+    [SerializeField] TMP_Text textLabel;
+    [SerializeField] DialogScriptable dialog;
+    [SerializeField] float timeWait;
+  
+    public void Interact()
+    {
+        imageBox.SetActive(true);
+        StartCoroutine(waitInterDialogues());
+        
+
+     
+        
+      
+    }
+
+    IEnumerator waitInterDialogues()
+    {
+        for (int i = 0; i < dialog.messages.Length; i++)
+        {
+          
+            textLabel.text = dialog.messages[i].text;
+           
+            yield return new WaitForSeconds(timeWait);
+
+
+        }
+      
+
+       
+
+    }
+
+}
+
