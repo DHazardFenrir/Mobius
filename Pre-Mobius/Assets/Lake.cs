2b117a5e11805a9fc6406be99e41b5d6e028ec80
Gold <dzharzard96@gmail.com>
2021-06-23 20:24:03 -0500

    Shader de Awa con un cheat para que se mueva de arriba a abajo. Ahorita le meto una mejora.

---
+++ Pre-Mobius/Assets/Lake.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lake : MonoBehaviour
+{
+    public GameObject lake;
+    public float speed;
+
+    public void Update()
+    {
+        float y = Mathf.PingPong(Time.time * speed, 1) * 4 - 2;
+        lake.transform.position = new Vector3(0, y, 0);
+    }
+}
