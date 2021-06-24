96529f36b5728776aaa04b2566a9707fcdb13db7
alanrdz19 <alanrdz19@hotmail.com>
2021-06-24 08:10:54 -0500

    Cambios minimos en los scripts

--- Pre-Mobius/Assets/Mobius/Scripts/Puzzles/Puzzle1/FirstPuzzle.cs
+++ Pre-Mobius/Assets/Mobius/Scripts/Puzzles/Puzzle1/FirstPuzzle.cs
@@ -4,90 +4,31 @@ using UnityEngine;
 
 public class FirstPuzzle : MonoBehaviour
 {
-    [SerializeField] PuzzleData puzzleData;
 
-    [SerializeField] bool[] step;
+    [SerializeField] bool puzzleCompleted;
+
+    [SerializeField] Items ofrenda;
 
     Inventario inventario;
 
     private void Start()
     {
-        puzzleData = GetComponent<PuzzleData>();
         inventario = FindObjectOfType<Inventario>();
     }
 
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            CheckEvents();
-        }
-    }
-
-    //es m[as que nada para verificar que todo se este llevando a cabo aun cuando se hacen los pasos
-    void CheckEvents()
-    {
-        for(int i=0;i<puzzleData.puzzleEvents.Length;)
-        {
-            if(puzzleData.puzzleEvents[i].active)
-            {
-                i++;
-            }
-            else
-            {
-                Debug.Log("Aun no se completan todos los eventos");
-                break;
-            }
-            if(i==puzzleData.puzzleEvents.Length)
-            {
-                puzzleData.puzzleCompleted = true;
-            }
-        }
-        if(puzzleData.puzzleCompleted==true)
-        Debug.Log("Se completaron los eventos");
-    }
-
-    void CheckSteps()
-    {
-        for(int i=0;i<step.Length;)
-        {
-            if(step[0])
-            {
-                i++;
-            }
-            else
-            {
-                Debug.Log("Aun no se completan todos los pasos");
-                break;
-            }
-            if(i==step.Length)
-            {
-                puzzleData.puzzleCompleted = true;
-            }
-        }       
-    }
-
-    void StepOne()
-    {
-        //hablar granjero
-        step[0] = true;
-        CheckSteps();
-    }
-
-    void StepTwo()
+ 
+    public void CerrarPresa()
     {
         //Cerrar presa
-        step[1] = true;
-        CheckSteps();
     }
 
-    void FinalStep()
+    public void FinalStep()
     {
         // obtener ofrenda
-        if(inventario.inventory.Contains(puzzleData.ofrenda))
+        if(inventario.inventory.Contains(ofrenda))
         {
-            step[3] = true;
+            puzzleCompleted = true;
         }
-        CheckSteps();
+
     }
 }
--- Pre-Mobius/Assets/Mobius/Scripts/Puzzles/PuzzleData.cs
+++ Pre-Mobius/Assets/Mobius/Scripts/Puzzles/PuzzleData.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class PuzzleData : MonoBehaviour
 {
-    public Evento[] puzzleEvents;
-
     public bool puzzleCompleted;
 
     public Items ofrenda;
-
 }
