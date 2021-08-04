f7d69e9afccaab48447b0d379343286b1d7b9aaf
alanrdz19 <alanrdz19@hotmail.com>
2021-08-04 03:32:14 -0500

    Cambios al puzzle 3, hay que arreglar unas cosas del pick up

--- Pre-Mobius/Assets/Mobius/Scripts/Puzzles/Puzzle3/OfrendaHades.cs
+++ Pre-Mobius/Assets/Mobius/Scripts/Puzzles/Puzzle3/OfrendaHades.cs
@@ -8,6 +8,7 @@ public class OfrendaHades : MonoBehaviour, IInteractable
     [SerializeField] GameObject[] frutas;
     [SerializeField] Items[] frutasItems;
     [SerializeField] bool[] frutaColocada;
+    bool puzzleCompleted;
     Inventario inventory;
 
     private void Start()
@@ -20,16 +21,55 @@ public class OfrendaHades : MonoBehaviour, IInteractable
         ColocarFruta();
     }
 
+
     void ColocarFruta()
     {
         for(int i=0;i<frutas.Length;i++)
         {
-            if(inventory.inventory.Contains(frutasItems[i]) && frutaColocada[i]==false)
+            Debug.Log("1");
+            if (inventory.inventory.Contains(frutasItems[i]) && frutaColocada[i]==false)
             {
+                Debug.Log("2");
                 frutas[i].SetActive(true);
+                Debug.Log("3");
                 frutaColocada[i]= true;
+                Debug.Log("4");
+                ComprobarFrutero();
+            }
+            else
+            {
+                continue;
+            }           
+        }
+    }
+
+    void ComprobarFrutero()
+    {
+        for(int i=0;i<frutaColocada.Length;)
+        {
+            if(frutaColocada[i]==true)
+            {
+                i++;
+            }
+            else
+            {
+                //aqui te mueres
+                break;
+            }
+
+            if(i<frutaColocada.Length)
+            {
+                puzzleCompleted = true;
+                Debug.Log("Completado");
+                GetComponent<BoxCollider>().enabled = false;
+            }
+            else
+            {
+                //aqui te mueres tambien
+                break;
             }
         }
+
     }
 
 }
