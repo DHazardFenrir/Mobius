3bca1940f575e0739acd8eddabf2973106e07bea
alanrdz19 <alanrdz19@hotmail.com>
2021-10-28 03:21:49 -0500

    Inundación hecha y arreglado lo de la poción

--- Pre-Mobius/Assets/Mobius/Scripts/Puzzles/Puzzle1/Cadenas.cs
+++ Pre-Mobius/Assets/Mobius/Scripts/Puzzles/Puzzle1/Cadenas.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class Cadenas : MonoBehaviour, IInteractable
 {
-    [SerializeField] GameObject inundacion;
-    [SerializeField] GameObject presaVieja;
-    [SerializeField] GameObject nuevaPresa;
+    [SerializeField] Animator anim;
     [SerializeField] GameObject altar;
 
      void Start()
     {
-        inundacion = GameObject.FindGameObjectWithTag("Inundacion");
-        presaVieja = GameObject.FindGameObjectWithTag("PresaRota");
-        nuevaPresa = GameObject.FindGameObjectWithTag("PresaCompleta");
+        
     }
     public void Interact()
     {
@@ -23,10 +19,8 @@ public class Cadenas : MonoBehaviour, IInteractable
     void DryLake()
     {
         Debug.Log("enter");
-        inundacion.SetActive(false);
-        presaVieja.SetActive(false);
+        anim.SetBool("Active", false);
         altar.GetComponent<BoxCollider>().enabled = true;
-        nuevaPresa.SetActive(true);
         Debug.Log("Dry Lake");
     }
 }
