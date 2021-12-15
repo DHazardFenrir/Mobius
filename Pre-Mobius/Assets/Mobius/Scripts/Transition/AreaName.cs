1b8434c50510e9699c8d3924322d73ef5bfcee97
Gold <dzharzard96@gmail.com>
2021-12-14 21:58:49 -0600

    Quedo lo de los letreros.

---
+++ Pre-Mobius/Assets/Mobius/Scripts/Transition/AreaName.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Area", menuName = "Areas", order = 1)]
+
+public class AreaName : ScriptableObject
+{
+   public string nombre;
+}
